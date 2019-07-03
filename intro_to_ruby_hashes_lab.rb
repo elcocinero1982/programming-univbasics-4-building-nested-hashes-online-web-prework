8808ac575f2063ad5f9f8cb761b2dbdec81e39f2
jose luis <cortezluisjose@gmail.com>
2019-07-03 03:13:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,40 +1,4 @@
 def monopoly
-	monopoly = {
-	  :railroads => 4
-	  {
-	    :pieces => 4,
-	    :rent_in_dollars => 12.5
-	    {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
-	    }
-	    :names => "railroads"
-	    {
-	      :reading_railroad => 2
-	      {
-	        "mortgage_value" => "$100"
-	      },
-	      :pensilvania_railroad => 4
-	      {
-	        "mortgage_value" => "$200"
-	      },
-	      :b_and_o_railroad => 6
-	      {
-	        "mortgage_value" => "$400"
-	      }
-	      :shortline => 8
-	      {
-	        "mortgage_value" => "$800"
-	      
-	      
-	      
-	      
-	    }
-	    }
-	  }
-	}
 
 end
 
